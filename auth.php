d6fdbd4ee1b2e740d502c14cdf43ae291af06d8a
Alexander Bias <alexander.bias@uni-ulm.de>
2016-03-07 11:36:24 +0100

    Add global to auth.php

--- auth.php
+++ auth.php
@@ -24,6 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+global $CFG;
+
 require_once($CFG->libdir.'/authlib.php');
 require_once($CFG->libdir.'/ldaplib.php');
 require_once($CFG->dirroot.'/user/lib.php');
