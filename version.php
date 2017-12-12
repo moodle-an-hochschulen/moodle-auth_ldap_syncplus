43b28ff6b9feef917b943dbab4084c4c08a0b205
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2017-12-12 13:53:04 +0100

    Prepare compatibility for Moodle 3.3, no functionality change.

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'auth_ldap_syncplus';
 $plugin->version = 2017063000;
 $plugin->release = 'v3.2-r4';
-$plugin->requires = 2016120500;
+$plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array('auth_ldap' => 2016120500);
+$plugin->dependencies = array('auth_ldap' => 2017051500);
