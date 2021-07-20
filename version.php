3e894cc62d21ee26c9959a3b5200d77bcedede4d
moodle-mw <michael.wuttke@hu-berlin.de>
2021-07-20 10:16:27 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_ldap_syncplus';
-$plugin->version = 2020121100;
+$plugin->version = 2021072000;
 $plugin->release = 'v3.11-r1';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
